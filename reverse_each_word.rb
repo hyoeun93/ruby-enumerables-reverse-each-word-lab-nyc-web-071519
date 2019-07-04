8ccb2cb8ef6c024e9755c302073df801b4cb7294
HyoEun Lee <hyoeun93@gmail.com>
2019-07-04 03:03:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   split_string = string.split(" ") 
   reversed = []
   split_string.each{
-    |string| reversed << string.reverse}
+    |elem| reversed << elem.reverse}
   return reversed.join(" ")
 end 
